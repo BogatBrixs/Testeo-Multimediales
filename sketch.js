d7b1d3e2c91b9d952559be3c937173e5736c5990
BogatBrixs <132857599+BogatBrixs@users.noreply.github.com>
2023-06-04 23:20:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+
+//----VARIABLES GLOBALES----//
+
+//variable para crear a la serpiente
+let s;
+
+//escala el tamaño de la grilla/matriz y tamaño de los objetos
+let scl = 25;
+
+
+let food;
+
+
+
+
+
+function setup() {
+  createCanvas(1000, 600);
+
+  s = new Snake();
+  pickLocation();
+
+  frameRate(10);
+
+}
+
+
+//-------DETECTAR LA FRUTA----------//
+function pickLocation() {
+  let cols = floor(width/scl);
+  let rows = floor(height/scl);
+
+  //comida
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+
+//----SIRVE PARA TESTEAR----//
+/*function mousePressed() {
+  s.total++; 
+}*/
+
+
+
+function draw() {
+  background(51);
+  
+  //si se come la fruta, se genera una nueva posicion
+  if (s.eat(food)) {
+    pickLocation();
+  }
+
+  //orden de funciones en la serpiente
+  s.death();
+  s.update();
+  s.show();
+  
+  //comida
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+
+//----MOVIMIENTO----//
+function keyPressed() {
+  if (keyCode === UP_ARROW){
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
---
+++ snake.js
@@ -0,0 +1,79 @@
+
+class Snake{
+
+
+    x = 0;
+    y = 0;
+    xspeed = 1;
+    yspeed = 0;
+    total = 0;
+    tail = [];
+
+
+    //----------------------//
+    eat = function(pos) {
+        let d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //----------------------//
+    dir = function(x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+
+    //----------------------//
+    death = function() {
+        for (let i = 0; i < this.tail.length; i++) {
+            let pos = this.tail[i];
+            let d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+
+                this.total = 0;
+                this.tail = [];
+            }
+            
+        }
+    }
+ 
+    //----------------------//
+    update = function() {
+
+    //recorre el arreglo de "cuadritos" generando el largo de la serpiente
+        if(this.total === this.tail.length){
+            for (let i = 0; i < this.tail.length-1; i++){
+            this.tail[i] = this.tail[i+1];
+            }
+        }
+        this.tail[this.total - 1] = createVector(this.x, this.y)
+
+      this.x += this.xspeed*scl;
+      this.y += this.yspeed*scl;
+        
+
+      //limitar el movimiento hasta los bordes
+      this.x = constrain(this.x, 0, width-scl);
+      this.y = constrain(this.y, 0, height-scl);
+
+
+    }
+  
+    //----------------------//
+    show = function() {
+        
+      fill(255);
+
+      //dibuja la serpiente 
+      for (let i = 0; i < this.total; i++){
+        rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+      rect(this.x, this.y, scl, scl);
+    }
+  }
